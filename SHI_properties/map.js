8182645784c4717d57d0441245e99fddfc5c39d7
Stephen Lead <stephen.lead@gmail.com>
2018-06-29 15:45:21 -0500

    add Stamen basemap

--- SHI_properties/map.js
+++ SHI_properties/map.js
@@ -1,36 +1,26 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+var center = [12.2, -84.9];
+var zoom = 6;
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: 'mapbox.streets'
-}).addTo(mymap);
+var tileOptions = {
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png',
+  opacity: 0.5
+};
+var basemap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', tileOptions);
 
-L.marker([51.5, -0.09]).addTo(mymap)
-  .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+var map = new L.Map("map", {
+  center: center,
+  zoom: zoom,
+  layers: [basemap],
+  zoomControl: false,
+  attributionControl: false
+});
 
-L.circle([51.508, -0.11], 500, {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5
-}).addTo(mymap).bindPopup("I am a circle.");
-
-L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-]).addTo(mymap).bindPopup("I am a polygon.");
-
-
-var popup = L.popup();
-
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(mymap);
-}
-
-mymap.on('click', onMapClick);
+var properties = L.esri.featureLayer({
+  url: "https://services1.arcgis.com/XBDCraMz4XwnRrFo/arcgis/rest/services/SHI_Properties/FeatureServer/0",
+  style: function () {
+    return { color: "#70ca49", weight: 2 };
+  }
+}).addTo(map);
