83412e2b34d2bf6fb9a42226d48a25817fbf15cb
Stephen Lead <stephen.lead@gmail.com>
2020-02-22 18:06:11 +1100

    add JR Pass demo v1

---
+++ JRPass/config.js
@@ -0,0 +1,14 @@
+var config = {
+  token: 'pk.eyJ1IjoianJwYXNzIiwiYSI6InFGWkZRZGcifQ.63K0wzE7UKeqvDSR9pWrcg',
+  layerIDs:
+  {
+    "shinkansen-stations-aocc91": "name_en",
+    "jr-important-stations-aigi7g (1)": "name_en",
+    "jr-stations-9rycem": "name_en",
+    // "shinkansen-lines-9ilxc9": "line_name_",
+    // "jr-lines-important-0vb5jj": "line_nam_1",
+    // "jr-lines-cbj1bi": "line_nam_1",
+    "nonjr-named-stations-9qd9k3": "name_en",
+    // "nonjr-named-lines-995u3z": "name_en"
+  }
+}
