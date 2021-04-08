27fcbd9d1e26b2f76dfbe7cc5cb6307b157b7312
Stephen Lead <stephen.lead@gmail.com>
2021-04-08 10:46:28 +1000

    add M7 Business Hub, fetch polygons by name temporarily

--- Goodman-leasing-mockup/index.js
+++ Goodman-leasing-mockup/index.js
@@ -1,13 +1,15 @@
 let map;
-let baseUrl = 'https://smartspace.goodman.com/arcgis/rest/services/PropertyBoundariesTemplate/FeatureServer/0'
+let propertyUrl = 'https://smartspace.goodman.com/arcgis/rest/services/PropertyBoundariesTemplate/FeatureServer/0';
+let enrichUrl = 'https://smartspace.goodman.com/arcgis/rest/services/Hosted/Leasing_enriched_drivetimes/FeatureServer/0';
 
 function initMap() {
 
   // retrieve the lat/long/zoom from the data-elements
-  var latitude = $("#title").data('latitude');
-  var longitude = $("#title").data('longitude')
-  var zoom = $("#title").data('zoom') || 15;
-  var objectid = $("#title").data('objectid');
+  let latitude = $("#title").data('latitude');
+  let longitude = $("#title").data('longitude')
+  let zoom = $("#title").data('zoom') || 15;
+  let objectid = $("#title").data('objectid');
+  let name = $("#title").data('name');
 
   if (latitude && longitude && objectid){
     map = new google.maps.Map(document.getElementById("map"), {
@@ -15,9 +17,10 @@ function initMap() {
       zoom: zoom,
     });
 
-    let url = baseUrl + '/query?outFields=*&returnGeometry=true&f=geojson';
-    url += '&where=objectid=' + objectid;
-    
+    // Fetch the GeoJSON representation of the property and load it into the map
+    let url = propertyUrl + '/query?outFields=*&returnGeometry=true&f=geojson';
+    // url += '&where=objectid=' + objectid;
+    url += "&where=name=%27" + name.replaceAll(" ", "%20") + "%27";
     map.data.loadGeoJson(url);
   } else {
     console.error("Can't find all required property details so map won't be shown")
