26b666e1148990b27c3ea3337250cc6a0b46792f
Stephen Lead <stephen.lead@gmail.com>
2021-05-04 20:07:08 +1000

    added oAuth test

---
+++ oAuth/app.js
@@ -0,0 +1,53 @@
+require([
+    "esri/views/MapView",
+    "esri/WebMap",
+    "esri/widgets/LayerList",
+    "esri/portal/Portal",
+    "esri/identity/OAuthInfo",
+    "esri/identity/IdentityManager",
+    "esri/portal/PortalQueryParams"
+    ], 
+    function(
+        MapView,
+        WebMap,
+        LayerList,
+        Portal,
+        OAuthInfo,
+        esriId,
+        PortalQueryParams
+    ) {
+
+    const info = new OAuthInfo({
+        appId: "JNywHQSYkzrfoxdw",
+        // portalUrl: "https://<host>:<port>/arcgis"
+        popup: false
+    });
+    esriId.registerOAuthInfos([info]);
+
+    esriId
+        .checkSignInStatus(info.portalUrl + "/sharing")
+        .then((status) => {
+            console.log("signed in status", status)
+        })
+        .catch(() => {
+            console.log("not signed in")
+        });
+
+    var webmap = new WebMap({
+      portalItem: {
+        id: "c1edd5c42b684a9cb8a00cec8ff8c859"
+      }
+    });
+
+    var view = new MapView({
+      map: webmap,
+      container: "viewDiv"
+    });
+
+    view.when(function() {
+        var layerList = new LayerList({
+            view: view
+        });
+        view.ui.add(layerList, "top-right");
+    });
+});
